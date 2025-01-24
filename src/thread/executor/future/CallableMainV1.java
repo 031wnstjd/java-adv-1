c12e26791cd2f83e9ad39c36e47b470e6f37ed91
Junsung <031wnstjd@naver.com>
2025-01-24 18:15:10 +0900

    Future1 - 소개

--- src/thread/executor/future/CallableMainV1.java
+++ src/thread/executor/future/CallableMainV1.java
@@ -1,22 +1,31 @@
 package thread.executor.future;
 
-import java.util.concurrent.Callable;
+import java.util.Random;
+import java.util.concurrent.*;
 
 import static thread.utils.MyLogger.log;
+import static thread.utils.ThreadUtils.sleep;
 
 public class CallableMainV1 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Future<Integer> future = es.submit(new MyCallable());
+        Integer result = future.get();
+        log("result value = " + result);
+        es.shutdown();
     }
 
     static class MyCallable implements Callable<Integer> {
 
         @Override
-        public Integer call() throws Exception {
+        public Integer call() {
             log("Callable 시작");
+            sleep(2000);
+            int value = new Random().nextInt(10);
+            log("create value = " + value);
             log("Callable 완료");
-            return 0;
+            return value;
         }
     }
 }
